99012e8f33342e53ba723e91ef43d172b9b962a3
DJacobson17 <83249575+DJacobson17@users.noreply.github.com>
2023-04-05 13:16:05 -0500

    added simple_forms, and changed duration to integer in flights table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_16_161447) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_05_181122) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
@@ -21,7 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_16_161447) do
     t.integer "departure_airport_id"
     t.integer "arrival_airport_id"
     t.datetime "departure"
-    t.time "duration"
+    t.integer "duration"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
