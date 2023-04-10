d92209de934e0d17169fa53af1636a45e3306ed7
DJacobson17 <83249575+DJacobson17@users.noreply.github.com>
2023-04-10 09:41:26 -0500

    created passenger model

---
+++ db/migrate/20230410144016_create_passengers.rb
@@ -0,0 +1,12 @@
+class CreatePassengers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :passengers do |t|
+      t.references :flight, null: false, foreign_key: true
+      t.references :booking, null: false, foreign_key: true
+      t.string :name
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_10_142644) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_10_144016) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
@@ -33,5 +33,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_10_142644) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "passengers", force: :cascade do |t|
+    t.integer "flight_id", null: false
+    t.integer "booking_id", null: false
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_passengers_on_booking_id"
+    t.index ["flight_id"], name: "index_passengers_on_flight_id"
+  end
+
   add_foreign_key "bookings", "flights"
+  add_foreign_key "passengers", "bookings"
+  add_foreign_key "passengers", "flights"
 end
