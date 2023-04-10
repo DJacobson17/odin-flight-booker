db33c956a939ead805f78b8810e430f2ed40d708
DJacobson17 <83249575+DJacobson17@users.noreply.github.com>
2023-04-10 09:33:37 -0500

    created booking model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_05_181122) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_10_142644) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "flight_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flight_id"], name: "index_bookings_on_flight_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -26,4 +33,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_05_181122) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "bookings", "flights"
 end
